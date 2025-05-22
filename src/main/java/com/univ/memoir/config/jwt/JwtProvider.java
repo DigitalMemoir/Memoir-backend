c601d19b38304151db52c05f8307426ba6f83f68
hoyeon <hoyeonying@gmail.com>
2025-05-23 07:35:49 +0900

    [feat] JWT 토큰 발급을 위한 JwtProvider 구현

---
+++ src/main/java/com/univ/memoir/config/jwt/JwtProvider.java
@@ -0,0 +1,30 @@
+package com.univ.memoir.config.jwt;
+
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final long expiration = 1000L * 60 * 60 * 24; // 1일
+
+    public String createToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+}
