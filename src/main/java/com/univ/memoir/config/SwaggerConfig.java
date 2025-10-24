588268add060059a0de9be35396de69362110576
hoyeon <hoyeonying@gmail.com>
2025-10-25 04:56:00 +0900

    [ci] ECR-EC2 CI/CD 파이프라인 및 Dockerfile, Gradle 설정 추가

--- src/main/java/com/univ/memoir/config/SwaggerConfig.java
+++ src/main/java/com/univ/memoir/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.univ.memoir.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +11,14 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://memoir.asia", description = "운영 서버"),
+                @Server(url = "http://localhost:8888", description = "개발 서버")
+        }
+)
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI openAPI() {
         String securitySchemeName = "bearerAuth";
@@ -19,7 +26,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Collec API Document")
-                        .version("v0.0.1")
+                        .version("v0.0.2")
                         .description("collec API 문서입니다."))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
